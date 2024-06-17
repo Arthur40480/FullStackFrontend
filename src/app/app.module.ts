8c73d508e57f9419a922a5868a31495f26ed167d
Arthur Gibert <arthurgibert@free.fr>
2024-06-17 14:36:05 +0200

    Initialisation de la nav

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,14 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { ComponentsComponent } from './hotels/components/components.component';
+import { HotelsComponent } from './components/hotels/hotels.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ComponentsComponent,
+    HotelsComponent
   ],
   imports: [
     BrowserModule,
