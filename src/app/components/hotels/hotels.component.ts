1a56492d0e91a2e9fe2f33de3e5d3c893ad49a7d
Arthur Gibert <arthurgibert@free.fr>
2024-06-18 10:39:18 +0200

    Formulaire de recherche OK

--- src/app/components/hotels/hotels.component.ts
+++ src/app/components/hotels/hotels.component.ts
@@ -14,8 +14,10 @@ export class HotelsComponent implements OnInit {
   error = null;
   listCity: City[] | undefined;
   listHotel: Hotel[] | undefined;
+  listFilteredHotels: any[] = [];
   idSelectedCity: number | null = null;
   nameCitySelected: string = '';
+  keyword: string = '';
 
   constructor(private apiService: ApiService, private cityService: CityService) { 
   
@@ -30,21 +32,27 @@ export class HotelsComponent implements OnInit {
     }
   }
 
+  
   /**
-  * Récupère toutes les villes en utilisant le service API et met à jour la liste des villes.
-  */
-  getAllCities() {
+   * Récupère toutes les villes en utilisant le service API et met à jour la liste des villes.
+   */
+  getAllCities(): void {
     this.apiService.getCities().subscribe({
       next: (data) => (this.listCity = data),
       error: (err) => (this.error = err.message),
       complete: () => (this.error = null)
-    })
+    });
   }
 
-  getAllHotel() {
+  /**
+   * Récupère tous les hôtels en utilisant le service API et met à jour la liste des hôtels.
+   * Réinitialise la ville sélectionnée à 'Toutes' et efface la ville sélectionnée du service CityService.
+   */
+  getAllHotel(): void {
     this.apiService.getAllHotel().subscribe({
       next: (data) => {
         this.listHotel = data;
+        this.listFilteredHotels = data;
         this.idSelectedCity = 0; 
         this.nameCitySelected = "Toutes";
         this.cityService.clearSelectedIdCity();
@@ -52,19 +60,49 @@ export class HotelsComponent implements OnInit {
       },
       error: (err) => (this.error = err.message),
       complete: () => (this.error = null)
-    })
+    });
   }
 
-  getAllHotelByCity(id: number, name: string) {
+  /**
+   * Récupère tous les hôtels par ville en utilisant le service API et met à jour la liste des hôtels.
+   * Définit la ville sélectionnée dans le service CityService.
+   * @param id ID de la ville sélectionnée
+   * @param name Nom de la ville sélectionnée
+   */
+  getAllHotelByCity(id: number, name: string): void {
     this.apiService.getHotelByCity(id).subscribe({
       next: (data) => (this.listHotel = data),
       error: (err) => (this.error = err.message),
       complete: () => (this.error = null),
-    })
-      this.cityService.setSelectedIdCity(id);
-      this.cityService.setSelectedNameCity(name);
-      this.nameCitySelected = name;
-      this.idSelectedCity = id;
+    });
+    this.cityService.setSelectedIdCity(id);
+    this.cityService.setSelectedNameCity(name);
+    this.nameCitySelected = name;
+    this.idSelectedCity = id;
   }
 
+  /**
+   * Filtre les hôtels en fonction du mot-clé saisi dans la barre de recherche.
+   */
+  filterHotel(): void {
+    if (this.keyword === '') {
+      this.listHotel = this.listFilteredHotels; 
+    } else {
+      this.listHotel = this.listFilteredHotels?.filter((hotel) =>
+        hotel.name.toLowerCase().includes(this.keyword)
+      );
+    }
+  }
+
+  /**
+   * Déclenchée lorsqu'une recherche est effectuée à partir de l'interface utilisateur.
+   * Met à jour le mot-clé de recherche et applique le filtre sur les hôtels.
+   */
+  onSearch(): void {
+    let recherche: string = (
+      document.getElementById('rechercheHotel') as HTMLInputElement
+    ).value.toLowerCase();
+    this.keyword = recherche;
+    this.filterHotel();
+  }
 }
--- src/app/services/api.service.ts
+++ src/app/services/api.service.ts
@@ -3,6 +3,7 @@ import { HttpClient } from '@angular/common/http';
 import { environment } from 'src/environments/environment';
 import { City } from '../models/city.model';
 import { Hotel } from '../models/hotel.model';
+import { Observable } from 'rxjs';
 
 @Injectable({
   providedIn: 'root'
@@ -13,17 +14,26 @@ export class ApiService {
 
   /**
    * Récupère toutes les villes depuis l'API.
-   * @returns Un tableau de villes.
-  */
-  public getCities() {
-    return this.http.get<City[]>(environment.host + '/cities');
+   * @returns Un Observable contenant un tableau de villes.
+   */
+  public getCities(): Observable<City[]> {
+    return this.http.get<City[]>(`${environment.host}/cities`);
   }
 
-  public getAllHotel() {
-    return this.http.get<Hotel[]>(environment.host + '/hotels');
+  /**
+   * Récupère tous les hôtels depuis l'API.
+   * @returns Un Observable contenant un tableau d'hôtels.
+   */
+  public getAllHotel(): Observable<Hotel[]> {
+    return this.http.get<Hotel[]>(`${environment.host}/hotels`);
   }
 
-  public getHotelByCity(id:number) {
-    return this.http.get<Hotel[]>(environment.host + '/hotels/' + id);
+  /**
+   * Récupère tous les hôtels d'une ville spécifique depuis l'API.
+   * @param id L'ID de la ville pour laquelle récupérer les hôtels.
+   * @returns Un Observable contenant un tableau d'hôtels de la ville spécifiée.
+   */
+  public getHotelByCity(id: number): Observable<Hotel[]> {
+    return this.http.get<Hotel[]>(`${environment.host}/hotels/${id}`);
   }
 }
